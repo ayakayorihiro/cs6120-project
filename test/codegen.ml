8169f784b0659791ac6a7dfbdb29f9723df1c91c
Ayaka Yorihiro <ay436@cornell.edu>
2022-05-17 09:20:38 -0400

    coden compiles rn

--- test/codegen.ml
+++ test/codegen.ml
@@ -8,6 +8,7 @@ let context = create_context ()
 let llvm_module = create_module context "brawn"
 let builder = builder context
 let named_values: (string, llvalue) Hashtbl.t = Hashtbl.create 10
+let _ = named_values
 
 let brawn_value_type = named_struct_type context "struct.brawn.brawn_value"
 let brawn_type = pointer_type brawn_value_type
@@ -24,20 +25,6 @@ let codegen_function_proto name args =
   | None -> declare_function name ft llvm_module
   | Some _ -> raise (CodeGenError "redefinition of function")
 
-let codegen_function_decl (Function (Identifier name, parameters, body)) =
-  (* probably don't want to clear out the named_values hashtable, 
-      as the guide says to. *)
-  let the_function = codegen_function_proto name parameters in
-  let bb = append_block context "entry" the_function in
-  position_at_end bb builder;
-  try
-    let ret_val = codegen_expr body in 
-    let _ = build_ret ret_val builder in
-    Llvm_analysis.assert_valid_function the_function;
-    the_function
-  with e ->
-    delete_function the_function;
-    raise e
   
 (* The operands e1 and e2 have already been reduced. 
    Now do the final binary tie-up.
@@ -67,8 +54,8 @@ let codegen_binary_expr expr e1 e2 =
 
 let rec codegen_expr expr = 
   match expr with
-  | Input i -> unimplemented (* todo get a helper involved *)
-  | FuncCall (name, args) -> unimplemented (* see Kaleidoscope guide *)
+  | Input _ -> unimplemented (* todo get a helper involved *)
+  | FuncCall _ -> unimplemented (* see Kaleidoscope guide *)
   | Plus (e1, e2) 
   | Subtract (e1, e2) 
   | Multiply (e1, e2)
@@ -92,39 +79,56 @@ let rec codegen_expr expr =
       let e1' = codegen_expr e1 in 
       let e2' = codegen_expr e2 in 
       codegen_binary_expr expr e1' e2'
-  | Regexp str -> unimplemented
-  | Ternary (e1, e2, e3) -> unimplemented (* const_select? *)
-  | Mem (e, array_name) -> unimplemented
-  | Negative e
-  | Positive e
-  | Not e -> unimplemented
-  | LIncr lvalue 
-  | LDecr lvalue
-  | RIncr lvalue
-  | RDecr lvalue
-  | LValue lvalue -> 
+  | Regexp _ -> unimplemented
+  | Ternary _ -> unimplemented (* const_select? *)
+  | Mem _ -> unimplemented
+  | Negative _
+  | Positive _
+  | Not _ -> unimplemented
+  | LIncr _ 
+  | LDecr _
+  | RIncr _
+  | RDecr _
+  | LValue _ -> 
       (* similarly, it makes sense to handle all these cases in one way,
          then take cases on expr to see what specific thing
          needs to be done for them *)
           unimplemented
   | Number f -> const_float brawn_type f
   | String s -> const_stringz context s
-  | PowAssign (lvalue, e)
-  | ModAssign (lvalue, e)
-  | MulAssign (lvalue, e)
-  | DivAssign (lvalue, e)
-  | AddAssign (lvalue, e)
-  | SubAssign (lvalue, e)
-  | Assignment (lvalue, e) -> 
+  | PowAssign (_, e)
+  | ModAssign (_, e)
+  | MulAssign (_, e)
+  | DivAssign (_, e)
+  | AddAssign (_, e)
+  | SubAssign (_, e)
+  | Assignment (_, e) -> 
       (* again, do it once and then disambiguate again *)
-      let e' = codegen_expr e in 
+      let e' = codegen_expr e in
+      let _ = e' in 
       (* ... *)
       unimplemented
 
-let codegen_stmt s = unimplemented
+let codegen_stmt _ = unimplemented
     
-let codegen_action_decl pattern body = unimplemented
+let codegen_action_decl _ _ = unimplemented
   (* if we match the pattern, run the body *)
+
+let codegen_function_decl (Function (Identifier name, parameters, body)) =
+  (* probably don't want to clear out the named_values hashtable, 
+      as the guide says to. *)
+  let the_function = codegen_function_proto name parameters in
+  let bb = append_block context "entry" the_function in
+  position_at_end bb builder;
+  try
+    let ret_val = codegen_stmt body in 
+    let _ = build_ret ret_val builder in
+    Llvm_analysis.assert_valid_function the_function;
+    the_function
+  with e ->
+    delete_function the_function;
+    raise e
+
 
 let codegen_item = function
   | FunctionDecl func -> codegen_function_decl func
@@ -133,6 +137,7 @@ let codegen_item = function
 (* TODO: this should be fleshed out further to take care of control flow etc *)
 let codegen_program ((Program items) : program) =
   List.map codegen_item items 
+  
   
 let id_function = 
   let decl = declare_function "id" (function_type brawn_type [|brawn_type|]) llvm_module in
@@ -144,6 +149,8 @@ let id_function =
   decl
 
 let emit_llvm () =
+let _ = codegen_expr in
+let _ = codegen_program in
   let _ = id_function in
   dump_module llvm_module
 
