a67fc9aaf5acf600b4fb5d8dea064bddaffa56ee
Ayaka Yorihiro <ay436@cornell.edu>
2022-05-17 19:00:14 -0400

    typechecking

--- lib/typecheck/typecheck.ml
+++ lib/typecheck/typecheck.ml
@@ -16,9 +16,9 @@ let functions = Hashtbl.create 10
 (* Whether a function is correctly called. *)
 let verify_call f n = 
     if not (Hashtbl.mem functions f) then 
-      raise (TypeCheckError "brawn: function does not exist.") else ();
-    if (Hashtbl.find functions f) != n then
-      raise (TypeCheckError "brawn: function called with incorrect number of arguments.") else ()
+      raise (TypeCheckError "brawn: function never defined.") else ();
+    if (Hashtbl.find functions f) > n then
+      raise (TypeCheckError "brawn: too many arguments in call.") else ()
 
 (* Whether or not to record an identifier in the globals *)
 let record_global i ig = if List.mem i ig then ()
@@ -56,40 +56,39 @@ and visit_expr ig = function
     | Assignment (l, e) -> visit_lvalue ig l; (visit_expr ig) e
     | Literal l -> Hashtbl.add constants l true
 
-and visit_stmt ig = function
-    | If (e, t, None) -> (visit_expr ig) e;
-                         visit_stmt ig t
-    | If (e, t, Some f) -> (visit_expr ig) e; 
-                           visit_stmt ig t; 
-                           visit_stmt ig f
+and visit_stmt ig loop func = function
+    | If (e, t, f) -> visit_expr ig e; 
+                      visit_stmt ig loop func t; 
+                      ignore (Option.map (visit_stmt ig loop func) f)
     | While (e, s)
-    | Do (s, e) -> visit_stmt ig s;
+    | Do (s, e) -> visit_stmt ig true func s;
                    visit_expr ig e
-    | For (is, e, us, s) -> ignore (Option.map (visit_stmt ig) is);
+    | For (is, e, us, s) -> ignore (Option.map (visit_stmt ig true func) is);
                             ignore (Option.map (visit_expr ig) e);
-                            ignore (Option.map (visit_stmt ig) us);
-                            visit_stmt ig s
-    | RangedFor (_, _, s) -> visit_stmt ig s
+                            ignore (Option.map (visit_stmt ig true func) us);
+                            visit_stmt ig true func s
+    | RangedFor (_, _, s) -> visit_stmt ig true func s
     | Exit (Some e)
-    | Return (Some e)
     | Expression e -> visit_expr ig e
+    | Return e -> if not func
+                  then raise (TypeCheckError "brawn: return outside function body.")
+                  else ignore (Option.map (visit_expr ig) e)
     | Print es -> ignore (List.map (visit_expr ig) es)
     | Delete (i, es) -> record_global i ig;
                         ignore (List.map (visit_expr ig) es)
-    | Block ss -> ignore (List.map (visit_stmt ig) ss)
-    | Return (None)
-    | Exit (None)
-    | Break
-    | Continue 
+    | Block ss -> ignore (List.map (visit_stmt ig loop func) ss)
+    | Exit None
     | Next
     | Skip -> ()
+    | Break -> if not loop then raise (TypeCheckError "brawn: break outside of loop.") else ()
+    | Continue -> if not loop then raise (TypeCheckError "brawn: continue outside of loop.") else ()
 
-let visit_func (Function (_, args, s)) = ignore (Option.map (visit_stmt args) s)
+let visit_func (Function (_, args, s)) = ignore (Option.map (visit_stmt args false true) s)
 
 let record_func (Function (f, args, _)) = Hashtbl.add functions f (List.length args)
 
 let visit_action (Action (p, s)) = ignore (Option.map visit_pattern p);
-                                   ignore (Option.map (visit_stmt []) s)
+                                   ignore (Option.map (visit_stmt [] false false) s)
 
 (* Extract constants from a Brawn program. *)
 let visit_program (Program (f, a)) = ignore (List.map record_func f);
